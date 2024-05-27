a0744e798fa9b7453fe4db123f7ab52f7e3f0c04
Apsan <np03cs4a220109@heraldcollege.edu.np>
2024-05-27 19:17:35 +0545

    chore: Adjust height of sidebar and main content area

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,26 +8,29 @@ import './index.css'
 import WatchingVideo from './components/WatchingVideo.jsx'
 
 const WatchingVideoComponent = () => {
-  return(
+  return (
     <React.StrictMode>
-    <Navbar />
-    <div className="flex flex-1 h-full">
-      <div className="sidebar w-[180px] h-[93vh] border-r-2">
-        <Sidebar />
+      <Navbar />
+      <div className="flex h-[calc(100vh-64px)]"> {/* Adjusted height for full viewport */}
+        <div className="sidebar w-[180px] h-full border-r-2">
+          <Sidebar />
+        </div>
+        <div className="main w-full h-full flex flex-col items-center justify-start overflow-y-auto">
+          <WatchingVideo />
+        </div>
       </div>
-    <WatchingVideo />
-    </div>
     </React.StrictMode>
   );
 }
 
 const HomePageComponent = () => {
-  return(
+  return (
     <React.StrictMode>
-    <App />
+      <App />
     </React.StrictMode>
   );
 }
+
 const RouterPaths = () => {
   return (
     <Router>
