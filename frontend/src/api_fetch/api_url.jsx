f408bf0d892949b986274bc90614e7c7a082d0c1
Apsan <np03cs4a220109@heraldcollege.edu.np>
2024-05-28 09:28:31 +0545

    chore: Refactor API URL fetching and error handling

--- frontend/src/api_fetch/api_url.jsx
+++ frontend/src/api_fetch/api_url.jsx
@@ -1,42 +1,11 @@
-async function fetchApiUrl() {
-    const response = await fetch("http://localhost:3000/apiurl");
-    const data = await response.json();
-    return data.apiUrl;
-}
-
-async function ApiWorking(apiUrl) {
+export default async function getAPIURL() {
     try {
-        const response = await fetch(`${apiUrl}/users/1`, {
-            method: 'GET',
-            headers: {
-                'ngrok-skip-browser-warning': 'true', // Add this header
-            }
-        });
-        return true;
-    }
-    catch (error) {
-        console.error('Error fetching users:', error);
-        return false;
-    }
-}
-
-async function main() {
-    try {
-        const goturl = await fetchApiUrl();
-        console.log("API URL is set to " + goturl);
-        const isApiWorking = await ApiWorking(goturl);
-        if (isApiWorking) {
-            console.log("API is working correctly.");
-
-            return goturl;
-        } else {
-            console.log("API URL is not set correctly.");
-            return "http://localhost:3000";
-        }
+        const response = await fetch("http://localhost:3000/apiurl");
+        const data = await response.json();
+        console.log("API URL fetched:", data.apiUrl); // Print response to console
+        return data.apiUrl;
     } catch (error) {
-        console.error("Error:", error);
+        console.error('Error fetching API URL:', error);
+        return "https://healthy-muskox-shortly.ngrok-free.app";
     }
 }
-
-main();
-export default main;
